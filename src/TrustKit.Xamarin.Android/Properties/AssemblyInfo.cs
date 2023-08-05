177c379b1adb2f19f7703a549c1631d0f532980d
Alexander DiBenedetto <alexander.dibenedetto@gmail.com>
2023-08-05 00:09:41 -0700

    Updated assembly info properties.

--- src/TrustKit.Xamarin.Android/Properties/AssemblyInfo.cs
+++ src/TrustKit.Xamarin.Android/Properties/AssemblyInfo.cs
@@ -1,3 +1,7 @@
 ﻿using System.Reflection;
+using Android;
+[assembly: LinkerSafe]
 [assembly: AssemblyTitle("TrustKit.Xamarin.Android")]
+[assembly: AssemblyDescription("TrustKit bindings for Xamarin.Android")]
 [assembly: AssemblyProduct("TrustKit.Xamarin.Android")]
+[assembly: AssemblyVersion("1.0.0.0")]
--- src/TrustKit.Xamarin.iOS.Bindings/Properties/AssemblyInfo.cs
+++ src/TrustKit.Xamarin.iOS.Bindings/Properties/AssemblyInfo.cs
@@ -3,10 +3,7 @@
 using Foundation;
 
 [assembly: LinkerSafe]
-
-[assembly: AssemblyTitle("TrustKit.Xamarin.iOS")]
+[assembly: AssemblyTitle("TrustKit.Xamarin.iOS.Bindings")]
 [assembly: AssemblyDescription("TrustKit bindings for Xamarin.iOS")]
-[assembly: AssemblyProduct("TrustKit.Xamarin.iOS")]
-[assembly: AssemblyCulture("en-US")]
-
+[assembly: AssemblyProduct("TrustKit.Xamarin.iOS.Bindings")]
 [assembly: AssemblyVersion("1.0.0.0")]
